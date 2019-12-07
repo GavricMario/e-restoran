98182198d00b5c465e19a42c2c39824ad7b3b7da
Mario Gavric <mario.gavric@globaldizajn.hr>
2019-12-07 16:27:45 +0100

    Initial Commit

---
+++ app/src/main/java/hr/fer/grupa/erestoran/MainActivity.kt
@@ -0,0 +1,12 @@
+package hr.fer.grupa.erestoran
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
