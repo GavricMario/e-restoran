4859938b7e32afe40e186b3a1df52d5ca4e268bd
Mario <mario.gavric@fer.hr>
2019-12-10 18:59:26 +0100

    Login and register done
    Firebase implemented, needs testing

--- app/src/main/java/hr/fer/grupa/erestoran/MainActivity.kt
+++ app/src/main/java/hr/fer/grupa/erestoran/MainActivity.kt
@@ -1,12 +1,25 @@
 package hr.fer.grupa.erestoran
 
+import android.content.Intent
+import android.content.Intent.ACTION_VIEW
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, SignInActivity::class.java))
+        finish()
     }
 }
