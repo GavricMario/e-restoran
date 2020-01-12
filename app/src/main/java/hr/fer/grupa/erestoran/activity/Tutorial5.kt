cae2db62b20a41a4cc7f4a51119932ed17b83c4d
Domagoj Jurenec <domagoj.jurenec@fer.hr>
2020-01-12 19:33:48 +0100

    tutorial done

--- app/src/main/java/hr/fer/grupa/erestoran/activity/Tutorial5.kt
+++ app/src/main/java/hr/fer/grupa/erestoran/activity/Tutorial5.kt
@@ -3,6 +3,8 @@ package hr.fer.grupa.erestoran.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import hr.fer.grupa.erestoran.R
 import hr.fer.grupa.erestoran.R.layout.activity_tutorial_5
 import kotlinx.android.synthetic.main.activity_tutorial_5.*
@@ -14,6 +16,13 @@ class Tutorial5 : AppCompatActivity() {
         setContentView(activity_tutorial_5)
 
         val prefs = getSharedPreferences(resources.getString(R.string.app_name), MODE_PRIVATE)
+
+
+        var  mAnimation =AnimationUtils.loadAnimation(this, R.anim.shake)
+        mAnimation.setRepeatCount(10)
+
+        mobitel.startAnimation(mAnimation)
+
 
         povratak.setOnClickListener{
             prefs.edit().putBoolean("firstUse", true).apply()
